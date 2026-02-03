cba05dc079755c3830eae1982de6fd106d03d353
Allyson Xavier <allysonxavier3@gmail.com>
2026-02-03 16:12:28 -0300

    feat(api): add status endpoint and PostgreSQL integration
    - Implemented `/api/v1/status` integration test to return HTTP 200.
    - Added a database client for PostgreSQL using `pg` v8.11.3.
    - Updated `package-lock.json` with required dependencies for database and linting.

---
+++ src/app/api/v1/status/route.js
@@ -0,0 +1,7 @@
+import database from "../../../../infra/database.js"
+
+export async function GET() {
+    const result = await database.query("SELECT 1 + 1 as sum");
+    console.log(result.rows)
+    return Response.json({ chave: "são acima da média" })
+}
