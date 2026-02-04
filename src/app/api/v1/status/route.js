cd7dcb7ecb3c9183c39f1b86d86968b1a0242ea5
Allyson Xavier <allysonxavier3@gmail.com>
2026-02-04 15:37:12 -0300

    feat(api): include database version in status endpoint
    - Updated `/api/v1/status` to return database version under `dependencies.database.version`.
    - Added integration test to validate the presence and value of the database version field.

--- src/app/api/v1/status/route.js
+++ src/app/api/v1/status/route.js
@@ -1,7 +1,16 @@
 import database from "infra/database.js"
 
 export async function GET() {
+    const updatedAt = new Date().toISOString()
+    const databaseValueResult = await database.query("SHOW server_version;")
+    const databaseVersionValue = databaseValueResult.rows[0].server_version
+    console.log(updatedAt, databaseVersionValue)
     return Response.json({
-        updated_at: new Date().toISOString(),
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                version: databaseVersionValue
+            }
+        }
     })
 }
