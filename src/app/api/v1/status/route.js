bb428972428131ee9778968b19a9ef557b60de58
Allyson Xavier <allysonxavier3@gmail.com>
2026-02-04 16:29:50 -0300

    feat(api): add opened_connections to status endpoint
    - Enhanced `/api/v1/status` to include `opened_connections` under `dependencies.database`.
    - Updated integration test to validate the presence and value of `opened_connections`.
    - Adjusted `.env.development` to align database name with test queries.

--- src/app/api/v1/status/route.js
+++ src/app/api/v1/status/route.js
@@ -9,13 +9,17 @@ export async function GET() {
     const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
     const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
 
-    console.log(Response.body)
+    const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
+
+    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+
     return Response.json({
         updated_at: updatedAt,
         dependencies: {
             database: {
                 version: databaseVersionValue,
-                max_connections: parseInt(databaseMaxConnectionsValue)
+                max_connections: parseInt(databaseMaxConnectionsValue),
+                opened_connections: databaseOpenedConnectionsValue
             }
         }
     })
