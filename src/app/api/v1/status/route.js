e1c28005e3bf2abb2ce58cc828f6674082294030
Allyson Xavier <allysonxavier3@gmail.com>
2026-02-04 15:52:36 -0300

    feat(api): add max_connections to status endpoint
    - Enhanced `/api/v1/status` to include `max_connections` under `dependencies.database`.
    - Updated integration test to validate the presence and value of `max_connections`.

--- src/app/api/v1/status/route.js
+++ src/app/api/v1/status/route.js
@@ -2,14 +2,20 @@ import database from "infra/database.js"
 
 export async function GET() {
     const updatedAt = new Date().toISOString()
+
     const databaseValueResult = await database.query("SHOW server_version;")
     const databaseVersionValue = databaseValueResult.rows[0].server_version
-    console.log(updatedAt, databaseVersionValue)
+
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
+    const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+
+    console.log(Response.body)
     return Response.json({
         updated_at: updatedAt,
         dependencies: {
             database: {
-                version: databaseVersionValue
+                version: databaseVersionValue,
+                max_connections: parseInt(databaseMaxConnectionsValue)
             }
         }
     })
