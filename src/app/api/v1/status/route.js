3c188abd64de76d2144104274431ff0c531cb432
Allyson Xavier <allysonxavier3@gmail.com>
2026-02-05 16:34:38 -0300

    feat(infra): improve database query handling and enhance status endpoint
    - Wrapped database queries in `try-catch-finally` blocks for better error handling and resource cleanup.
    - Used parameterized queries to securely fetch `opened_connections` based on the database name.

--- src/app/api/v1/status/route.js
+++ src/app/api/v1/status/route.js
@@ -7,9 +7,15 @@ export async function GET() {
     const databaseVersionValue = databaseValueResult.rows[0].server_version
 
     const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
+
+    const databaseName = process.env.POSTGRES_DB;
+
     const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
 
-    const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
+    const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName],
+    });
 
     const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
--- src/infra/database.js
+++ src/infra/database.js
@@ -8,10 +8,15 @@ async  function query (queryObject) {
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
     })
-    await client.connect()
-    const result = await client.query(queryObject)
-    await client.end()
-    return result
+
+    await client.connect();
+    try {
+        return await client.query(queryObject);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.end();
+    }
 }
 
 export default {
