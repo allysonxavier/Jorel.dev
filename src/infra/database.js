31b8e92a67868bed3ad2973c03bed9fcd8abde06
Allyson Xavier <allysonxavier3@gmail.com>
2026-02-10 18:36:25 -0300

    feat(infra): add ssl configuration for database connections
    - Enabled SSL for database connections in non-development environments.
    - Enhanced error handling by re-throwing errors caught during query execution.

--- src/infra/database.js
+++ src/infra/database.js
@@ -7,13 +7,15 @@ async  function query (queryObject) {
         user: process.env.POSTGRES_USER,
         password: process.env.POSTGRES_PASSWORD,
         database: process.env.POSTGRES_DB,
+        ssl: process.env.NODE_ENV !== "development",
     })
 
-    await client.connect();
     try {
+        await client.connect();
         return await client.query(queryObject);
     } catch (error) {
         console.error(error);
+        throw error;
     } finally {
         await client.end();
     }
